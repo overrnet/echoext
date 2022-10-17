2fa844083b07d0c9e674a6e06e6dec20f1fa9e1f
overrnet <brodyover@gmail.com>
2022-10-17 04:56:27 -0400

    init

---
+++ echoext.go
@@ -0,0 +1,70 @@
+package echoext
+
+import (
+	"crypto/subtle"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+)
+
+func BasicErrorHandler() echo.HTTPErrorHandler {
+	return func(c echo.Context, err error) {
+		if err, ok := err.(*echo.HTTPError); ok {
+			c.String(err.Code, fmt.Sprint(err.Message))
+			return
+		}
+		c.NoContent(http.StatusInternalServerError)
+	}
+}
+
+func CorsAny() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Response().Header().Set(echo.HeaderAccessControlAllowOrigin, "*")
+			return next(c)
+		}
+	}
+}
+
+type UserPass map[string]string
+
+func BasicAuth(kv UserPass) echo.MiddlewareFunc {
+	return middleware.BasicAuth(func(_ echo.Context, username, password string) (bool, error) {
+		passwordCheck, ok := kv[username]
+		if !ok {
+			return false, nil
+		}
+
+		if subtle.ConstantTimeCompare([]byte(password), []byte(passwordCheck)) == 1 {
+			return true, nil
+		}
+		return false, nil
+	})
+}
+
+type DisallowList []string
+
+func DisallowInPath(list DisallowList, code int) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			for _, txt := range list {
+				if strings.Contains(c.Request().URL.Path, txt) {
+					return echo.NewHTTPError(code, http.StatusText(code))
+				}
+			}
+			return next(c)
+		}
+	}
+}
+
+func EnvPortOr(port string) (string, bool) {
+	envPort, envExists := os.LookupEnv("PORT")
+	if !envExists {
+		return ":" + port, false
+	}
+	return ":" + envPort, true
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/overrnet/echoext
+
+go 1.19
+
+require github.com/labstack/echo/v5 v5.0.0-20220717203827-74022662be4a
+
+require (
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/time v0.0.0-20211116232009-f0f3c7e86c11 // indirect
+)
